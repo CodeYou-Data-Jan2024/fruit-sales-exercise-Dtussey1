73fcceb7869ef0e5498e2c936072288e9365ddc3
Daniel Tussey <tusseydaniel1@gmail.com>
2024-05-12 13:45:58 -0400

    Second Commit

--- fruitsales.py
+++ fruitsales.py
@@ -4,6 +4,6 @@ data = {'Apples': [35, 41],'Bananas': [21, 34]}
 
 index = ['2017 Sales', '2018 Sales']
 
-df = pd.DataFrame(data, index=index)
-
+df = pd.DataFrame(data, index)
+df.to_csv("fruit.csv")
 print(df)
